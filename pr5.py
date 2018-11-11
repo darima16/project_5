816e71fadc78122d3ec021d3bd15abd6ade53688
ms.darima2000@mail.ru <ms.darima2000@mail.ru>
2018-11-11 23:41:47 +0700

    commit

--- pr5.py
+++ pr5.py
@@ -3,6 +3,12 @@
 В качестве подсказки для расшифровки текста и решение самой задачи Алексей Григорьевич оставил зеркало.
 Помогите бедному школьнику хотя бы прочесть условие задачи, ведь у него дедлайн до понедельника. '''
 
+file = input('Введите имя файла: ')
+try:
+    input_file = open(file)
+except FileNotFoundError:
+    print('Файл {} не найден'.format(file))
+    exit()
 with open('input.txt') as f_in:
     with open('output.txt', 'w') as f_out:
         for line in f_in:
